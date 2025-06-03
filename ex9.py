7f85c6db7b9652c8f690563e371baffe0b86a647
Gui <guilherme.visualdev@gmail.com>
2025-06-03 04:07:23 -0300

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,4 @@
+quadrados_pares = [x**2 for x in range(0, 101) if x % 2 == 0]                   # x**2 = quadrado (número x ele mesmo)
+                                                                                #range = de 0 a 101   
+
+print(quadrados_pares)
